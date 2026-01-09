514058f6b5ed4db4c0d19404708e88972e3e7bb2
user <email>
2026-01-09 08:35:23 +0400

    a1

--- pipeline/pandas/0-from_numpy.py
+++ pipeline/pandas/0-from_numpy.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
+"""Docstring for 0-from_numpy.py."""
 
 import pandas as pd
 
-"""
-    DocString for 0-from_numpy.py
-"""
-
 
 def from_numpy(seed):
-
-    """
-        DocString for from_numpy()
-    """
-
+    """Docstring for from_numpy."""
     alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 
     return pd.DataFrame(seed, columns=list(alphabet[:seed.shape[1]]))
