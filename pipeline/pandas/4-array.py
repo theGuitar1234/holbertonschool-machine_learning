4da38546b4941fb57de40d00b6fd2189131f27d5
user <email>
2026-01-09 09:47:07 +0400

    a1

--- pipeline/pandas/4-array.py
+++ pipeline/pandas/4-array.py
@@ -1,10 +1,6 @@
 #!/usr/bin/env python3
 """Docstring for 4-array.py."""
 
-import pandas as pd
-import numpy as np
-
-
 def array(df):
     """Docstring for array."""
     return df[["High", "Close"]].tail(10).values
