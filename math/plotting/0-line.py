9e48185fcc5494327e0aa3a8294f2e02bb254344
user <email>
2026-01-09 15:52:15 +0400

    a1

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -11,8 +11,8 @@ def line():
     """Docstring for line."""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
-    plt.plot(y, color='red')
     plt.xlim(0, 10)
+    plt.plot(y, color='red')
     plt.show()
 
 
