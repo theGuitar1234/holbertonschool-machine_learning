4db539aeb030ed105c1a4d16bd2d271b984dc148
user <email>
2026-01-09 16:42:05 +0400

    a1

---
+++ math/plotting/4-frequency.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Docstring for 4-frequency.py."""
+import numpy as np
+
+
+import matplotlib.pyplot as plt
+
+
+def frequency():
+    """Docstring for frequency."""
+    np.random.seed(5)
+    student_grades = np.random.normal(68, 15, 50)
+    plt.figure(figsize=(6.4, 4.8))
+    plt.ylim(0, 30)
+    plt.xlim(0, 100)
+    plt.title("Project A")
+    plt.xlabel("Grades")
+    plt.ylabel("Number of Students")
+    plt.hist(student_grades, bins=10, edgecolor="black") 
