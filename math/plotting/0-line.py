70c6ce4766eabb8b3a1e804938a85b489fc9b3a6
user <email>
2026-01-09 15:51:26 +0400

    a1

--- math/plotting/0-line.py
+++ math/plotting/0-line.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 """Docstring for 0-line.py."""
 
-
 import numpy as np
 
 
@@ -9,7 +8,7 @@ import matplotlib.pyplot as plt
 
 
 def line():
-    """Docstring line."""
+    """Docstring for line."""
     y = np.arange(0, 11) ** 3
     plt.figure(figsize=(6.4, 4.8))
     plt.plot(y, color='red')
