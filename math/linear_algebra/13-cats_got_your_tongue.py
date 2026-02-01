205463e6636354e1bd79ca60221d2c16a3560e12
theGuitar1234 <turanlizad776@gmail.com>
2026-02-01 17:37:20 +0400

    Create 13-cats_got_your_tongue.py

---
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Docstring for 13-cats_got_your_tongue.py."""
+import numpy as np
+
+
+def np_cat(mat1, mat2, axis=0):
+    """Docstring for np_cat."""
+    return np.concatenate((mat1, mat2), axis=axis)
