12b21210f7b33cfdb160770ee558bb1aafba397d
user <email>
2026-02-01 18:29:49 +0400

    a1

---
+++ math/linear_algebra/102-squashed_like_sardines.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+
+def _shape(mat):
+    sh = []
+    while isinstance(mat, list):
+        sh.append(len(mat))
+        mat = mat[0]
+    return tuple(sh)
+
+
+def _copy(mat):
+    if isinstance(mat, list):
+        return [_copy(x) for x in mat]
+    return mat
+
+
+def cat_matrices(mat1, mat2, axis=0):
+    if not isinstance(axis, int) or axis < 0:
+        return None
+
+    sh1 = _shape(mat1)
+    sh2 = _shape(mat2)
+
+    if axis >= len(sh1) or axis >= len(sh2):
+        return None
+
+    for d in range(len(sh1)):
+        if d == axis:
+            continue
+        if sh1[d] != sh2[d]:
+            return None
+
+    if axis == 0:
+        return [_copy(x) for x in mat1] + [_copy(x) for x in mat2]
+
+    result = []
+    for a, b in zip(mat1, mat2):
+        c = cat_matrices(a, b, axis=axis - 1)
+        if c is None:
+            return None
+        result.append(c)
+    return result
