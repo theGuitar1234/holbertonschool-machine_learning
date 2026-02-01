33bc146d70d4be76a274227981ce260946800a5b
user <email>
2026-02-01 18:02:02 +0400

    a1

--- math/linear_algebra/100-slice_like_a_ninja.py
+++ math/linear_algebra/100-slice_like_a_ninja.py
@@ -2,12 +2,11 @@
 """Docstring for 100-slice_like_a_ninja.py."""
 
 
-import numpy as np
-
-
 def np_slice(matrix, axes={}):
     """Docstring for np_slice."""
+    result = []
     for key in axes.keys():
-        for i in matrix:
+        for i in range(len(matrix)):
             s = axes[key]
-            print(i[slice(*s)])
+            result.append(matrix[i][slice(*s)].tolist())
+    return result
