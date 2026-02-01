1b482659caaebf3124942ed0cbbe53c13b7edae7
theGuitar1234 <turanlizad776@gmail.com>
2026-02-01 17:36:24 +0400

    Create 12-bracin_the_elements.py

---
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Docstring for 12-bracin_the_elements.py."""
+
+
+def np_elementwise(mat1, mat2):
+    """Docstring for no_elementwise."""
+    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
