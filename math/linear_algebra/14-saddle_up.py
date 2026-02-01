881be42c53fb962c17d690f574101d7c365babc5
theGuitar1234 <turanlizad776@gmail.com>
2026-02-01 17:38:29 +0400

    Create 14-saddle_up.py

---
+++ math/linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Docstring for 14-saddle_up.py."""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """Docstring for no_matmul."""
+    return np.matmul(mat1, mat2)
