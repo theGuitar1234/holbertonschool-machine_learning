4dbf64761ad3e2e78676ca7151173ca925238ad0
Turan Alizada <turanlizad776@gmail.com>
2026-02-08 21:17:25 +0400

    Create 3-adjugate.py

---
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""Docstring for numpy.3-adjugate.py."""
+
+
+def adjugate(mat):
+    """Docstring for adjugate."""
+    if (not isinstance(mat, list) or len(mat) == 0 or
+            not all(isinstance(row, list) for row in mat)):
+        print("matrix must be a list of lists")
+        return
+    if len(mat) == 1 and len(mat[0]) == 0:
+        return 1
+    n = len(mat)
+    if any(len(row) != n for row in mat):
+        print("matrix must be a square matrix")
+        return
+    if len(mat) == 1:
+        return [[1]]
+    result = []
+    res = [[0 for _ in range(len(mat[0]))] for _ in range(len(mat))]
+    for r in range(len(mat)):
+        for i in range(len(mat)):
+            chunk = []
+            for j in range(len(mat)):
+                if (j != r):
+                    temp = []
+                    for k in range(len(mat)):
+                        if (k != i):
+                            temp.append(mat[j][k])
+                    chunk.append(temp)
+            result.append(chunk)
+            res[i][r] = determinant(chunk) * (-1)**(i+r)
+    return res
+
+
+def determinant(mat):
+    """Docstring for determinant."""
+    if (not isinstance(mat, list) or len(mat) == 0 or
+            not all(isinstance(row, list) for row in mat)):
+        print("matrix must be a list of lists")
+        return
+    if len(mat) == 1 and len(mat[0]) == 0:
+        return 1
+    n = len(mat)
+    if any(len(row) != n for row in mat):
+        print("matrix must be a square matrix")
+        return
+    if (len(mat) == 1):
+        # print("reached the end : ", mat)
+        return mat[0][0]
+    result = []
+    res = 0
+    for i in range(len(mat[0])):
+        chunk = []
+        for j in range(1, len(mat)):
+            temp = []
+            for k in range(len(mat[j])):
+                if (k != i):
+                    temp.append(mat[j][k])
+            chunk.append(temp)
+        result.append(chunk)
+        res += mat[0][i] * determinant(chunk) * (-1)**i
+    return res
