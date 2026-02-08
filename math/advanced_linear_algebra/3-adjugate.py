3c5ef8485e64e06c07a5ecef915e383884463cb3
Turan Alizada <turanlizad776@gmail.com>
2026-02-08 21:19:29 +0400

    Update 3-adjugate.py

--- math/advanced_linear_algebra/3-adjugate.py
+++ math/advanced_linear_algebra/3-adjugate.py
@@ -4,18 +4,22 @@
 
 def adjugate(mat):
     """Docstring for adjugate."""
-    if (not isinstance(mat, list) or len(mat) == 0 or
-            not all(isinstance(row, list) for row in mat)):
+    if (
+        not isinstance(mat, list)
+        or not all(isinstance(row, list) for row in mat)
+    ):
         print("matrix must be a list of lists")
         return
-    if len(mat) == 1 and len(mat[0]) == 0:
-        return 1
-    n = len(mat)
-    if any(len(row) != n for row in mat):
-        print("matrix must be a square matrix")
+    if (
+        len(mat) == 0
+        or any(len(row) == 0 for row in mat)
+        or any(len(row) != len(mat) for row in mat)
+    ):
+        print("matrix must be a non-empty square matrix")
         return
     if len(mat) == 1:
         return [[1]]
+
     result = []
     res = [[0 for _ in range(len(mat[0]))] for _ in range(len(mat))]
     for r in range(len(mat)):
