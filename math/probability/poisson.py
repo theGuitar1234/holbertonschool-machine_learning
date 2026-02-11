ce0edce193422ffdd8913a31c3822a9c50102efa
Turan Alizada <turanlizad776@gmail.com>
2026-02-11 09:07:54 +0400

    Create poisson.py

---
+++ math/probability/poisson.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+"""Docstring for poisson.py."""
+
+import math
+
+
+class Poisson:
+    """Docstring for Poisson."""
+
+    def __init__(self, data=None, lambtha=1.):
+        """Docstring for the constructor."""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError("data must be a list")
+            if len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            self.lambtha = float(sum(data) / len(data))
+
+    def pmf(self, k):
+        """Docstring for pmf"""
+        k = int(k)
+        if k < 0:
+            return 0
+        return (math.exp(-self.lambtha) * (self.lambtha ** k)) / math.factorial(k)
+
+    def cdf(self, k):
+        """Docstring for cdf"""
+        k = int(k)
+        if k < 0:
+            return 0
+        return sum(self.pmf(i) for i in range(k + 1))
