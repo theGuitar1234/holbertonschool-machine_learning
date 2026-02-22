fe08d9f0cc44d1e930f57bbefe35da6b9651fa1c
Turan Alizada <turanlizad776@gmail.com>
2026-02-22 17:44:29 +0400

    Create 1-build_decision_tree.py

---
+++ supervised_learning/decision_tree/1-build_decision_tree.py
@@ -0,0 +1,90 @@
+#!/usr/bin/env python3
+"""Docstring."""
+
+
+import numpy as np
+
+
+class Node:
+    """Docstring."""
+
+    def __init__(self,
+                 feature=None,
+                 threshold=None,
+                 left_child=None,
+                 right_child=None,
+                 is_root=False, depth=0):
+        """Docstring."""
+        self.feature = feature
+        self.threshold = threshold
+        self.left_child = left_child
+        self.right_child = right_child
+        self.is_leaf = False
+        self.is_root = is_root
+        self.sub_population = None
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Docstring."""
+        max_depth = self.depth
+
+        if self.left_child is not None:
+            max_depth = max(max_depth, self.left_child.max_depth_below())
+
+        if self.right_child is not None:
+            max_depth = max(max_depth, self.right_child.max_depth_below())
+
+        return max_depth
+
+
+    def count_nodes_below(self, only_leaves=False):
+        """Docstring."""
+        count = 0 if only_leaves else 1
+
+        if self.left_child is not None:
+            count += self.left_child.count_nodes_below(only_leaves=only_leaves)
+
+        if self.right_child is not None:
+            count += self.right_child.count_nodes_below(only_leaves=only_leaves)
+
+        return count
+
+
+class Leaf(Node):
+    """Docstring."""
+    def __init__(self, value, depth=None):
+        """Docstring."""
+        super().__init__()
+        self.value = value
+        self.is_leaf = True
+        self.depth = depth
+
+    def max_depth_below(self):
+        """Docstring."""
+        return self.depth
+
+
+class Decision_Tree():
+    """Docstring."""
+    def __init__(self,
+                 max_depth=10,
+                 min_pop=1,
+                 seed=0,
+                 split_criterion="random",
+                 root=None):
+        """Docstring."""
+        self.rng = np.random.default_rng(seed)
+        if root:
+            self.root = root
+        else:
+            self.root = Node(is_root=True)
+        self.explanatory = None
+        self.target = None
+        self.max_depth = max_depth
+        self.min_pop = min_pop
+        self.split_criterion = split_criterion
+        self.predict = None
+
+    def depth(self):
+        """Docstring."""
+        return self.root.max_depth_below()
