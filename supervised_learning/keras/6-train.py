4496631b00546632975103a74d2ad756a6276e4b
Turan Alizada <turanlizad776@gmail.com>
2026-03-15 11:52:03 +0400

    Update 6-train.py

--- supervised_learning/keras/6-train.py
+++ supervised_learning/keras/6-train.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""."""
+"""6-train.py."""
 
 import tensorflow.keras as K
 
@@ -7,7 +7,7 @@ import tensorflow.keras as K
 def train_model(network, data, labels, batch_size, epochs,
                 validation_data=None, early_stopping=False, patience=0,
                 verbose=True, shuffle=False):
-    """."""
+    """train_model."""
     callbacks = []
 
     if early_stopping and validation_data is not None:
