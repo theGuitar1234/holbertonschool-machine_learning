de438c4a3e9d326ac41526d82342e95d247855b6
Turan Alizada <turanlizad776@gmail.com>
2026-03-15 11:51:33 +0400

    Create 7-train.py

---
+++ supervised_learning/keras/7-train.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""7-train.py."""
+
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size, epochs,
+                validation_data=None, early_stopping=False, patience=0,
+                learning_rate_decay=False, alpha=0.1, decay_rate=1,
+                verbose=True, shuffle=False):
+    """train_model."""
+    callbacks = []
+
+    if validation_data is not None:
+        if early_stopping:
+            callbacks.append(
+                K.callbacks.EarlyStopping(
+                    monitor='val_loss',
+                    patience=patience
+                )
+            )
+
+        if learning_rate_decay:
+            def scheduler(epoch):
+                """scheduler."""
+                return alpha / (1 + decay_rate * epoch)
+
+            callbacks.append(
+                K.callbacks.LearningRateScheduler(
+                    scheduler,
+                    verbose=1
+                )
+            )
+
+    return network.fit(
+        data,
+        labels,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=validation_data,
+        callbacks=callbacks,
+        verbose=verbose,
+        shuffle=shuffle
+    )
