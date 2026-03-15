2d75dc8f1e94ae8ddbfcc2a3d17f74899e5a21c3
Turan Alizada <turanlizad776@gmail.com>
2026-03-15 12:08:40 +0400

    Create 13-predict.py

---
+++ supervised_learning/keras/13-predict.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Predicts something idk what but definitely does something."""
+
+import tensorflow.keras as K
+
+
+def predict(network, data, verbose=False):
+    """just look at what this returns duhhh."""
+    return network.predict(data, verbose=verbose)
